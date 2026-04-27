301c78ab42d14c0b8ce4f6852887fe0ef7751b72
Enzo Locatelli <enzort70290@gmail.com>
2026-04-27 11:07:07 +0200

    feat: add sitemap integration and enhance site-wide SEO metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind()],
+  site: 'https://enzolocatelli.fr',
+  integrations: [tailwind(), sitemap()],
   adapter: node({
     mode: 'standalone',
   }),
